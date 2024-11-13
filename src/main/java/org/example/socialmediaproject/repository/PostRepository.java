05e12c224402086424d85dbaab1aedeef188f777
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-11-12 22:13:01 -0300

    feat: add UserRepository with method findByUsername

--- src/main/java/org/example/socialmediaproject/repository/PostRepository.java
+++ src/main/java/org/example/socialmediaproject/repository/PostRepository.java
@@ -1,11 +1,13 @@
-package org.example.socialmediaproject.reprository;
+package org.example.socialmediaproject.repository;
 
 import org.example.socialmediaproject.model.PostModel;
 import org.example.socialmediaproject.model.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface PostRepository extends JpaRepository<PostModel, Long> {
-    PostModel findAllByUser (UserModel id);
+public interface PostRepository extends JpaRepository<PostModel, UUID> {
+    PostModel findAllByUser (UserModel userModel);
 }
