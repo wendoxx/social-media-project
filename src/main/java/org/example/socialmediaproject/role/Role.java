5698834a046d7712627106a7f14770a6134a675b
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-11-12 21:38:23 -0300

    feat: add role admin and role user

---
+++ src/main/java/org/example/socialmediaproject/role/Role.java
@@ -0,0 +1,17 @@
+package org.example.socialmediaproject.role;
+
+public enum Role {
+    ADMIN ("admin"),
+    USER ("user");
+
+    private String role;
+
+    Role(String role) {
+        this.role = role;
+    }
+
+    public String getRole() {
+        return role;
+    }
+}
+
